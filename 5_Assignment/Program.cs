92dd8d91b0fcbaa09f82ca3656399d99d8e4c6de
Flor908 <florentin908@gmail.com>
2022-08-15 10:42:41 +0200

    Asg 5

--- 5_Assignment/Program.cs
+++ 5_Assignment/Program.cs
@@ -1,21 +1,34 @@
-﻿//Un-comment below code and fix the issues
+//Un-comment below code and fix the issues
 //Push to github
-/*
-int RecursiveFibonacci(int first, int second, int count)
-{
+
+
+long RecursiveFibonacci(long first, long second, long count){
+
     count--;
-    
-    
+
     if(count == 0) {
-    
+        return second + first;
     }else{
-        return RecursiveFibonacci(?, ?, ?);
+        return RecursiveFibonacci(first + second, first, count);
     }
 }
 
-int lastFibo = RecursiveFibonacci(1,1,5);
-Console.WriteLine("Last fibo number was " + lastFibo);
-Console.ReadLine(); 
-*/
-
-
+//You can't any numbers below 1 or above 92, this will crash the program 
+while(true){
+    Console.Write("Fibonacci of what number?\n> ");
+    string text = Console.ReadLine();
+    if (text == ""){
+        Console.WriteLine("invalid input");
+    }else if (text.StartsWith("-")){
+       Console.WriteLine("You can't use negative numbers"); 
+    }else{
+        if(text.All(char.IsDigit)){
+            long number = long.Parse(text);
+            Console.WriteLine(number);
+            long lastFibo = RecursiveFibonacci(1,1,number); 
+            Console.WriteLine("Last fibo number was " + lastFibo);
+        }else{
+            Console.WriteLine("The Given String is Not a Number");
+        }
+    }
+}   
